3c496e0e6dbc992ca238bdafe3174178c42fbe39
gcerame <g.cerame@gmail.com>
2022-11-12 11:17:04 +0100

    crud

---
+++ crud/app/acciones.php
@@ -0,0 +1,54 @@
+<?php
+
+
+function accionDetalles($id)
+{
+    $usuario = $_SESSION['tuser'][$id];
+    $nombre = $usuario[0];
+    $login = $usuario[1];
+    $clave = $usuario[2];
+    $comentario = $usuario[3];
+    $orden = "Detalles";
+    include_once "layout/formulario.php";
+    exit();
+}
+
+function accionAlta()
+{
+    $nombre = "";
+    $login = "";
+    $clave = "";
+    $comentario = "";
+    $orden = "Nuevo";
+    include_once "layout/formulario.php";
+    exit();
+}
+
+function accionPostAlta()
+{
+
+    limpiarArrayEntrada($_POST); //Evito la posible inyección de código
+    $nuevo = [$_POST['nombre'], $_POST['login'], $_POST['clave'], $_POST['comentario']];
+    $_SESSION['tuser'][] = $nuevo;
+}
+
+function accionModificar($id)
+{
+    $usuario = $_SESSION['tuser'][$id];
+    $nombre = $usuario[0];
+    $login = $usuario[1];
+    $clave = $usuario[2];
+    $comentario = $usuario[3];
+    $orden = "Modificar";
+    include_once "layout/formulario.php";
+    exit();
+}
+
+function accionPostModificar () {
+    limpiarArrayEntrada($_POST);
+    $modificado = [$_POST['nombre'], $_POST['login'], $_POST['clave'], $_POST['comentario']];
+    $_SESSION['tuser']['nombre'] = $modificado;
+
+}
+
+
